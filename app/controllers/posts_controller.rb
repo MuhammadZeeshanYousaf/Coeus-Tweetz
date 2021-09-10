43ef64eb85bb158520bcf6b259dfb5be72df1e32
Zeeshan <zeeshanyousaf430@gmail.com>
2021-09-10 18:55:19 +0500

    post view edit created

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,22 @@
+class PostsController < ApplicationController
+
+  def index
+    @post = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    redirect_to "index"
+  end
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,3 @@
 class Post < ApplicationRecord
+  validates :tweet, presence: true
 end
