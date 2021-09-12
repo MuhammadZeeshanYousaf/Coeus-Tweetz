64714aec8214c198f399fd25e93c95cf89a72b98
Zeeshan <zeeshanyousaf430@gmail.com>
2021-09-12 05:27:47 +0500

    crud done, bootstrap design added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,18 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+//code to link bootstrap
+import 'bootstrap'
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+//------------------
+
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
