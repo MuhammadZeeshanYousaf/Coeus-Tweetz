ae506960f2ce04b545bb4f6c1a24ecd2ebb32bd6
Zeeshan <zeeshanyousaf430@gmail.com>
2021-09-15 19:15:43 +0500

    Some modifications comments and styles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_15_083324) do
+ActiveRecord::Schema.define(version: 2021_09_15_141420) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -33,8 +33,6 @@ ActiveRecord::Schema.define(version: 2021_09_15_083324) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id", null: false
-    t.integer "likes", default: 0
-    t.integer "comments", default: 0
     t.integer "comment_id"
     t.index ["comment_id"], name: "index_posts_on_comment_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
